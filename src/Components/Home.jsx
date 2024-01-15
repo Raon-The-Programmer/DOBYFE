10e57013be6fe5bceadfa904fe50617943b85e46
Rahul John <msdrahuljohn@gmail.com>
2024-01-16 00:00:44 +0530

    added register link for testing

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div><h3><strong>Home Page</strong></h3><Link to='/signup'>Register</Link></div>
+    
   )
 }
 
