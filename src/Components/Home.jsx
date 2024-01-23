415688c600064d8420d8a34406c47f85058b5d49
Rahul John <msdrahuljohn@gmail.com>
2024-01-23 19:19:06 +0530

    policy

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -29,8 +29,9 @@ const Home = () => {
       <motion.div initial={{opacity:0}} animate={{opacity:1}} transition={{duration:1,ease:'easeIn'}}>
         <h2 style={{fontWeight:'bolder'}}>DOBBY</h2>
       </motion.div>
-      <motion.div style={{marginTop:'2px'}} initial={{opacity:0}} animate={{opacity:1}} transition={{duration:'1',delay:1}}>
-       <p style={{fontFamily:'monospace'}}>Laundry Today / Naked Tommorow</p>
+      <motion.div style={{marginTop:'2px',display:'flex',flexDirection:'column',justifyContent:'center'}} initial={{opacity:0}} animate={{opacity:1}} transition={{duration:'1',delay:1}}>
+       <p style={{fontFamily:'monospace',position:'relative'}}>Laundry Today / Naked Tommorow</p>
+       <p style={{marginLeft:'3rem',position:'absolute',bottom:'0',marginBottom:'3rem'}}>By continuing you agree to our <br /> <span style={{color:'red',marginLeft:'5px'}}>Terms of Use <span style={{color:'black'}}>&</span> Privacy Policy</span></p>
       </motion.div>
     </div> : navigate('/signup') }
     </div>
