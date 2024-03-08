1ffbd604ed1b1e6a35e1a8b99f4373f94e5ead37
Rahul John <msdrahuljohn@gmail.com>
2024-03-09 00:31:47 +0530

    ec2 address added

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const baseurl = "http://localhost:3001"
-const baseurl = "35.170.182.146:3001"
+const baseurl = "https://doby.onrender.com"
 
 const authInstance = axios.create({
     baseURL:baseurl,
